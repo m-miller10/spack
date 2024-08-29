2c12edce7a7da2455ec25d07880c223b12ccb257
m-miller10 <124190040+m-miller10@users.noreply.github.com>
2024-08-29 16:45:08 -0700

    The default python build has an extended version of tkniter as it's default. Requesting python with the tkniter vairant seems to be what is causing the cyclic dependency. I simply removed the dependency on the variant and it built and ran properly (at least with my limited testing of the 3.0.0 version)

--- var/spack/repos/builtin/packages/py-pymol/package.py
+++ var/spack/repos/builtin/packages/py-pymol/package.py
@@ -13,8 +13,9 @@ class PyPymol(PythonPackage):
     movies. Open Source PyMOL is free to everyone!"""
 
     homepage = "https://pymol.org"
-    url = "https://github.com/schrodinger/pymol-open-source/archive/v2.4.0.tar.gz"
+    url = "https://github.com/schrodinger/pymol-open-source/archive/refs/tags/v3.0.0.tar.gz"
 
+    version("3.0.0", sha256="45e800a02680cec62dff7a0f92283f4be7978c13a02934a43ac6bb01f67622cf")
     version("2.5.0", sha256="aa828bf5719bd9a14510118a93182a6e0cadc03a574ba1e327e1e9780a0e80b3")
     version("2.4.0", sha256="5ede4ce2e8f53713c5ee64f5905b2d29bf01e4391da7e536ce8909d6b9116581")
     version("2.3.0", sha256="62aa21fafd1db805c876f89466e47513809f8198395e1f00a5f5cc40d6f40ed0")
@@ -22,10 +23,14 @@ class PyPymol(PythonPackage):
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
 
-    depends_on("python+tkinter@2.7:", type=("build", "link", "run"), when="@2.3.0:2.4.0")
-    depends_on("python+tkinter@3.6:", type=("build", "link", "run"), when="@2.5.0:")
+    # the default python build has an extended version of tkniter as it's default.
+    # requesting python with the tkniter vairant seems to be what is causing the cyclic dependency
+    # I simply removed the dependency on the variant and it built and ran properly (at least with 
+    # my limited testing of the 3.0.0 version)
+    depends_on("python@3.9:", type=("build", "link", "run"),)
     # in newer pip versions --install-option does not exist
-    depends_on("py-pip@:23.0", type="build")
+    #depends_on("py-pip@:23.0", type="build")
+    depends_on("py-setuptools@69.2.0:", type="build")
     depends_on("gl")
     depends_on("glew")
     depends_on("libpng")
@@ -41,7 +46,7 @@ class PyPymol(PythonPackage):
     depends_on("libmmtf-cpp", type=("build", "run", "link"))
     depends_on("msgpack-c", type=("build", "run"))
     depends_on("libpng", type=("build", "run"))
-    depends_on("py-numpy", type=("build", "link", "run"))
+    depends_on("py-numpy@1.26.4:2", type=("build", "link", "run"))
     depends_on("py-msgpack", type=("build", "run"))
 
     def install_options(self, spec, prefix):
